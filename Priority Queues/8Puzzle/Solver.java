c3eff39e236daaa6e05e481d085c11fe3e505804
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-12 11:19:52 +0300

    implemented constructor

--- Priority Queues/8Puzzle/Solver.java
+++ Priority Queues/8Puzzle/Solver.java
@@ -8,6 +8,48 @@ public class Solver {
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+	int moves = 0;
+        bNode = new JNode(initial, moves);
+        TwinNode = new JNode(initial.twin(), moves);
+        MinPQ<JNode> Queue = new MinPQ<>();
+        
+        //insert both
+        Queue.insert(bNode);
+        Queue.insert(TwinNode);
+        //choose nice one to solve
+        bNode = Queue.delMin();
+        
+        // till the very end...
+        while (!bNode.isGoal()) {
+            // make node neighbors stack
+            Stack<Board> neighbors = new Stack<Board>();
+            //and add all the neighbors for this node there
+            for (Board b : bNode.neighbors()) {
+                neighbors.push(b);
+            }
+
+            moves = bNode.moves + 1;
+
+            // now check each neighbor
+            for (Board b : neighbors) {
+                Board bPrevious = null;
+
+                // temp previous state
+                if (bNode.prev != null) {
+                    bPrevious = bNode.prev.getjBoard();
+                }
+                
+                // when find really NEW one, 
+                // add new node to Queue
+                if (!b.equals(bPrevious)) {
+                    JNode node = new JNode(b, moves);
+                    node.prev = bNode;
+                    Queue.insert(node);
+                }
+            }
+            // next node to proceed
+            bNode = Queue.delMin();
+        }
     }
 
     
