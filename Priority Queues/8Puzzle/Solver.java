bf4260e726460e4fdd7ea0f3d505391aa1143574
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-12 11:00:18 +0300

    puzzle solver skeleton

---
+++ Priority Queues/8Puzzle/Solver.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+
+public class Solver {
+	private JNode bNode, TwinNode;
+	
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+    }
+
+    
+    
+    // returns true if the initial board solvable
+    public boolean isSolvable() {
+    }
+
+    
+    
+    // min number of moves to solve initial board
+    public int moves() {
+    }
+
+    
+    
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+    }
+    
+    
+     private class JNode implements Comparable<JNode> {
+    }
+}
