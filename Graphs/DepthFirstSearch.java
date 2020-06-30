d7dea1918913fdb3350de3dd2cf929da46363cfe
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-30 17:43:24 +0300

    test client

--- Graphs/DepthFirstSearch.java
+++ Graphs/DepthFirstSearch.java
@@ -43,6 +43,17 @@ public class DepthFirstSearch {
 
     
     public static void main(String[] args) {
-                    StdOut.println("connected");
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        int s = Integer.parseInt(args[1]);
+        DepthFirstSearch search = new DepthFirstSearch(G, s);
+        for (int v = 0; v < G.V(); v++) {
+            if (search.marked(v))
+                StdOut.print(v + " ");
+        }
+
+        StdOut.println();
+        if (search.count() != G.V()) StdOut.println("NOT connected");
+        else                         StdOut.println("connected");
     }
 }
