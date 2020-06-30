f4df856374d53d25c39462774e3cfcd5514d06ca
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-30 15:13:48 +0300

    dfs skeleton

---
+++ DepthFirstSearch.java
@@ -0,0 +1,28 @@
+public class DepthFirstSearch {
+
+
+    public DepthFirstSearch(Graph G, int s) {
+
+    }
+
+
+    private void dfs(Graph G, int v) {
+
+    }
+
+
+    public boolean marked(int v) {
+
+    }
+
+
+    public int count() {
+        return count;
+    }
+
+
+
+    public static void main(String[] args) {
+                    StdOut.println("connected");
+    }
+}
