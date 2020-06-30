c7a8c744e71c22c0709263688c9c14878687e66a
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-30 15:15:10 +0300

    Update DepthFirstSearch.java

--- Graphs/DepthFirstSearch.java
+++ Graphs/DepthFirstSearch.java
@@ -1,5 +1,6 @@
 public class DepthFirstSearch {
-
+    private boolean[] marked;    // marked[v] = is there an s-v path?
+    private int count;           // number of vertices connected to s
 
     public DepthFirstSearch(Graph G, int s) {
 
