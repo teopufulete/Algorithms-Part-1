374b3adbefa72215387fc765954199c0fbd6ba56
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-01 17:57:06 +0300

    directed graph skeleton

---
+++ Graphs/Digraph.java
@@ -0,0 +1,79 @@
+//adjacency list directed graph implementation
+
+public class DirectedGraph {
+
+    
+    // Initializes an empty digraph with vertices.
+    public DirectedGraph(int vertices) {
+        
+    }
+     
+     
+    public DirectedGraph(In in) {
+      
+    }
+
+
+    // Initializes a new digraph that is a deep copy of the specified digraph.
+    public DirectedGraph(DirectedGraph G) {
+       
+    }
+      
+      
+    // Returns the number of vertices in this digraph.
+    public int vertices() {
+       
+    }
+
+
+    // Returns the number of edges in this digraph.
+    public int edges() {
+        
+    }
+
+
+    private void validateVertex(int vertex) {
+        
+    }
+
+
+    // Adds the directed edge vertex→destination to this digraph.
+    public void addEdge(int vertex, int destination) {
+
+    }
+
+
+    // Returns the vertices adjacent from vertex in this digraph.
+    public Iterable<Integer> adj(int vertex) {
+   
+    }
+
+
+    // Returns the number of directed edges incident from vertex 
+    public int outdegree(int vertex) {
+
+    }
+
+
+    // Returns the number of directed edges incident to vertex 
+    public int indegree(int vertex) {
+        
+    }
+
+
+    // Returns the reverse of the digraph.
+    public Digraph reverse() {
+        
+    }
+
+
+    // Returns a string representation of the graph.
+    public String toString() {
+
+    }
+
+    // Unit test client
+    public static void main(String[] args) {
+        
+    }
+}
