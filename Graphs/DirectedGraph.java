b3510a0c2405ffd86a76180c33c6588c584a2c0c
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-01 18:06:07 +0300

    Initializes an empty digraph with vertices

--- Graphs/DirectedGraph.java
+++ Graphs/DirectedGraph.java
@@ -1,19 +1,29 @@
 //adjacency list directed graph implementation
 
 public class DirectedGraph {
+    
     private static final String NEWLINE = System.getProperty("line.separator");
     private final int vertices;           // number of vertices in this digraph
     private int edges;                 // number of edges in this digraph
     private Bag<Integer>[] list;    // list[v] = adjacency list for vertex v
     private int[] indegree;        // indegree[v] = indegree of vertex v
     
+    
     // Initializes an empty digraph with vertices.
     public DirectedGraph(int vertices) {
-        
+        if (vertices < 0) throw new IllegalArgumentException("Number of vertices in a graph must be nonnegative");
+        this.vertices = vertices;
+        this.E = 0;
+        indegree = new int[vertices];
+        list = (Bag<Integer>[]) new Bag[vertices];
+        for (int vertex = 0; vertex < vertices; vertex++) {
+            list[vertex] = new Bag<Integer>();
+        }
     }
      
-     
-    public DirectedGraph(In in) {
+    
+    // Initializes a digraph from the specified input stream. 
+    public DirectedGraph(In input) {
       
     }
 
