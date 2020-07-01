c7793217c03e680a33140555788064bf11ce6d60
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-01 17:45:31 +0300

    implemented breath first search

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,6 +1,6 @@
-//adjacency list graph representation
 import java.util.LinkedList;
 
+// adjacency list graph representation
 class Graph { 
     private int vertex;   // No. of vertices 
     private LinkedList<Integer> list[]; // Adjacency Lists 
@@ -35,7 +35,36 @@ class Graph {
         }
     } 
   
-
+    // prints BFS traversal from a given source s 
+    void BFS(int s) { 
+        // Mark all the vertices as not visited(By default set as false) 
+        boolean visited[] = new boolean[vertex]; 
+  
+        // Create a queue for BFS 
+        LinkedList<Integer> queue = new LinkedList<Integer>(); 
+  
+        // Mark the current node as visited and enqueue it 
+        visited[s] = true; 
+        queue.add(s); 
+  
+        while (queue.size() != 0) { 
+            // Dequeue a vertex from queue and print it 
+            s = queue.poll(); 
+            System.out.print(s + " "); 
+  
+            // Get all adjacent vertices of the dequeued vertex s 
+            // If a adjacent has not been visited, then mark it visited and enqueue it 
+            Iterator<Integer> i = adj[s].listIterator(); 
+            while (i.hasNext()) { 
+                int n = i.next(); 
+                if (!visited[n]) { 
+                    visited[n] = true; 
+                    queue.add(n); 
+                } 
+            } 
+        } 
+    } 
+    
     public static void main(String args[]) { 
         
     } 
