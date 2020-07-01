3e2a3bd9e6a5b05f3b32b58ebce82b590a6993b1
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-01 17:05:26 +0300

    skeleton for bfs

---
+++ Graphs/Graph.java
@@ -0,0 +1,23 @@
+class Graph { 
+
+  
+    // Constructor 
+    Graph(int v) { 
+       
+    } 
+  
+    // Function to add an edge into the graph 
+    void addEdge(int v,int w) { 
+       
+    } 
+  
+    // prints BFS traversal from a given source s 
+    void BFS(int s) { 
+        
+    } 
+  
+    // Driver method to 
+    public static void main(String args[]) { 
+        
+    } 
+} 
