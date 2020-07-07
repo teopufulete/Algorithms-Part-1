6e1cb6aa375f225835bcb39b6b1b9b7cc887a756
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-07 14:05:34 +0300

    compute list of vertices in each cc n print

--- Graphs/ConnectedComponents.java
+++ Graphs/ConnectedComponents.java
@@ -96,6 +96,21 @@ public class ConnectedComponents {
         int m = cc.count();
         StdOut.println(m + " components");
 
-       
+        // compute list of vertices in each connected component
+        Queue<Integer>[] components = (Queue<Integer>[]) new Queue[m];
+        for (int i = 0; i < m; i++) {
+            components[i] = new Queue<Integer>();
+        }
+        for (int v = 0; v < G.V(); v++) {
+            components[cc.id(v)].enqueue(v);
+        }
+
+        // print results
+        for (int i = 0; i < m; i++) {
+            for (int v : components[i]) {
+                StdOut.print(v + " ");
+            }
+            StdOut.println();
+        }       
     }
 }
