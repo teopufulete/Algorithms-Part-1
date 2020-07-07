7da86aa29b5368491c51a74a303d5e69ca1f3d36
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-07 10:44:57 +0300

    constructor

--- Graphs/ConnectedComponents.java
+++ Graphs/ConnectedComponents.java
@@ -1,8 +1,18 @@
 public class ConnectedComponents {
-
     private boolean[] marked;   // marked[v] = has vertex v been marked?
     private int[] id;           // id[v] = id of connected component containing v
     private int[] size;         // size[id] = number of vertices in given component
     private int count;          // number of connected components
     
+    public ConnectedComponents(Graph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        size = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+                count++;
+            }
+        }
+    }
 }
