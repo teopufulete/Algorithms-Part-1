1ce93f67dfc26b8a1e39f92e2e6d0da6403ed684
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-07 10:58:54 +0300

    computes cc of weighted graphs

--- Graphs/ConnectedComponents.java
+++ Graphs/ConnectedComponents.java
@@ -4,7 +4,22 @@ public class ConnectedComponents {
     private int[] size;         // size[id] = number of vertices in given component
     private int count;          // number of connected components
     
+    // Computes the connected components of the undirected graph 
     public ConnectedComponents(Graph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        size = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+                count++;
+            }
+        }
+    }
+    
+    
+   // Computes the connected components of the edge-weighted graph 
+    public ConnectedComponents(EdgeWeightedGraph G) {
         marked = new boolean[G.V()];
         id = new int[G.V()];
         size = new int[G.V()];
