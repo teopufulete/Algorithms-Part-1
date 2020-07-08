dd0f7dffe0c5fa0f31bf9f2bfa114eb5c0d83775
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-08 10:36:08 +0300

    added constructor

--- Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
+++ Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 
 
 public class EdgeWeightedGraph {
@@ -7,19 +8,30 @@ public class EdgeWeightedGraph {
     private int E;
     private Bag<Edge>[] adjacent;
     
+    
     // Initializ an empty edge-weighted graph with v vertices and 0 edges.
     public EdgeWeightedGraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adjacent[v] = new Bag<Edge>();
+        }
     }
 
+    
     // Initialize a random edge-weighted graph with v vertices and E edges.
     public EdgeWeightedGraph(int V, int E) {
     }
+    
 
     // Initialize edge-weighted graph from an input stream.
     public EdgeWeightedGraph(In in) {
         
     }
 
+    
     // Initialize a new edge-weighted graph that is a deep copy of G
     public EdgeWeightedGraph(EdgeWeightedGraph G) {
 
@@ -31,38 +43,46 @@ public class EdgeWeightedGraph {
         return V;
     }
 
+    
     // Return the number of edges in graph
     public int E() {
         return E;
     }
 
+    
     private void validateVertex(int v) {
         
     }
 
+    
     // Adds the undirected edge e to graph.
     public void addEdge(Edge e) {
     }
 
+    
     // Returns the edges connected to vertex v
     public Iterable<Edge> adj(int v) {
     }
 
+    
     // Returns the degree of vertex v
     public int degree(int v) {
        
     }
 
+    
     // Return all edges 
     public Iterable<Edge> edges() {
        
     }
 
+    
     // String representation of the edge-weighted graph.
     public String toString() {
         
     }
 
+    
     // Unit tests 
     public static void main(String[] args) {
 
