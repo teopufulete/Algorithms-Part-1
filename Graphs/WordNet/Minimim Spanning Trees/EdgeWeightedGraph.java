e346b14da72574ff9415a44783095ddeb3b5ebaa
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-08 11:43:43 +0300

    return all edges

--- Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
+++ Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
@@ -129,7 +129,21 @@ public class EdgeWeightedGraph {
     
     // Return all edges 
     public Iterable<Edge> edges() {
-       
+        Bag<Edge> list = new Bag<Edge>();
+        for (int v = 0; v < V; v++) {
+            int selfLoops = 0;
+            for (Edge e : adj(v)) {
+                if (e.other(v) > v) {
+                    list.add(e);
+                }
+                // add only one copy of each self loop (self loops will be consecutive)
+                else if (e.other(v) == v) {
+                    if (selfLoops % 2 == 0) list.add(e);
+                    selfLoops++;
+                }
+            }
+        }
+        return list;
     }
 
     
