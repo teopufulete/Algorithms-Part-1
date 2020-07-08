efec17740e5a3041f85a742a6fb8a993a6bb4274
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-08 10:45:39 +0300

    make graph from input

--- Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
+++ Graphs/WordNet/Minimim Spanning Trees/EdgeWeightedGraph.java
@@ -37,6 +37,31 @@ public class EdgeWeightedGraph {
 
     // Initialize edge-weighted graph from an input stream.
     public EdgeWeightedGraph(In in) {
+        if (in == null) throw new IllegalArgumentException("argument is null");
+
+        try {
+            V = in.readInt();
+            adjacent = (Bag<Edge>[]) new Bag[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new Bag<Edge>();
+            }
+
+            int E = in.readInt();
+            if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                validateVertex(v);
+                validateVertex(w);
+                double weight = in.readDouble();
+                Edge e = new Edge(v, w, weight);
+                addEdge(e);
+            }
+        }   
+        
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in EdgeWeightedGraph constructor", e);
+        }
         
     }
 
