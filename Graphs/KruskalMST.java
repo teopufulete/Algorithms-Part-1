872fa42bb54188fb5cb6f33eac2a7d2fd578edbb
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-10 16:01:36 +0300

    unit test

--- Graphs/KruskalMST.java
+++ Graphs/KruskalMST.java
@@ -100,5 +100,12 @@ public class KruskalMST {
 
     // Unit tests 
     public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        KruskalMST mst = new KruskalMST(G);
+        for (Edge e : mst.edges()) {
+            StdOut.println(e);
+        }
+        StdOut.printf("%.5f\n", mst.weight());
     }
 }
