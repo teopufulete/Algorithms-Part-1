d2696d952cb4baa7df51408ed3b26905d53b6ded
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-16 14:16:27 +0300

    variables

--- Graphs/Minimum Spanning Trees/LazyPrimMST.java
+++ Graphs/Minimum Spanning Trees/LazyPrimMST.java
@@ -1,5 +1,12 @@
 public class LazyPrimMST {
+    private static final double FLOATING_POINT_EPSILON = 1E-12;
 
+    private double weight;       // total weight of MST
+    private Queue<Edge> mst;     // edges in the MST
+    private boolean[] marked;    // marked[v] = true iff v on tree
+    private MinPQ<Edge> priorityQueue;      // edges with one endpoint in tree
+    
+    
     // Compute a minimum spanning tree (or forest) of an edge-weighted graph.
     public LazyPrimMST(EdgeWeightedGraph G) {
         
