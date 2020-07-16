5daa4155757b6d451c14330efa52ccf27de50688
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-07-16 14:39:21 +0300

    constructor

--- Graphs/Minimum Spanning Trees/LazyPrimMST.java
+++ Graphs/Minimum Spanning Trees/LazyPrimMST.java
@@ -9,7 +9,14 @@ public class LazyPrimMST {
     
     // Compute a minimum spanning tree (or forest) of an edge-weighted graph.
     public LazyPrimMST(EdgeWeightedGraph G) {
-        
+        mst = new Queue<Edge>();
+        pq = new MinPQ<Edge>();
+        marked = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)     // run Prim from all vertices to
+            if (!marked[v]) prim(G, v);     // get a minimum spanning forest
+
+        // check optimality conditions
+        assert check(G);
     }
 
 
