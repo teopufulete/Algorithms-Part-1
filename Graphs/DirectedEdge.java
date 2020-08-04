14665a603cf4433d708796006b3be03518db04d1
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-04 13:36:52 +0300

    Create DirectedEdge.java

---
+++ Graphs/DirectedEdge.java
@@ -0,0 +1,35 @@
+public class DirectedEdge { 
+    private final int v;
+    private final int w;
+    private final double weight;
+
+  
+    public DirectedEdge(int v, int w, double weight) {
+
+    }
+    
+    
+    public int from() {
+
+    }
+
+
+    public int to() {
+        
+    }
+
+   
+    public double weight() {
+        
+    }
+
+    
+    public String toString() {
+        
+    }
+
+    //unit testing
+    public static void main(String[] args) {
+
+    }
+}
