f4c20c80a703424ea8b991b9619a9795101b8231
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-04 13:52:43 +0300

    constructor

--- Graphs/DirectedEdge.java
+++ Graphs/DirectedEdge.java
@@ -3,9 +3,14 @@ public class DirectedEdge {
     private final int w;
     private final double weight;
 
-  
-    public DirectedEdge(int v, int w, double weight) {
 
+    public DirectedEdge(int v, int w, double weight) {
+        if (v < 0) throw new IllegalArgumentException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IllegalArgumentException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
     }
     
     
