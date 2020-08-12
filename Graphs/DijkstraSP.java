6cd12a099e5472d0a6a88fe7b3cb6facb4186359
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-12 11:38:11 +0300

    constructor

--- Graphs/DijkstraSP.java
+++ Graphs/DijkstraSP.java
@@ -6,6 +6,32 @@ public class DijkstraSP {
 
     // computes a shortest-paths tree from source vertexs to every other vertex .
     public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        for (DirectedEdge e : G.edges()) {
+            if (e.weight() < 0)
+                throw new IllegalArgumentException("edge " + e + " has negative weight");
+        }
+
+        distTo = new double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+
+        validateVertex(s);
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // relax vertices in order of distance from s
+        pq = new IndexMinPQ<Double>(G.V());
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (DirectedEdge e : G.adj(v))
+                relax(e);
+        }
+
+        // check optimality conditions
+        assert check(G, s);
+    }
 
     }
 
