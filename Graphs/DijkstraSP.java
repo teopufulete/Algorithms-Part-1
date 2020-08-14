1f9f32cb9cab1cc9be378b85133cd5ef465457c6
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-14 13:52:17 +0300

    print shortest path

--- Graphs/DijkstraSP.java
+++ Graphs/DijkstraSP.java
@@ -137,5 +137,19 @@ public class DijkstraSP {
         
         // compute shortest paths
         DijkstraSP sp = new DijkstraSP(G, s);
+    
+        // print shortest path
+        for (int t = 0; t < G.V(); t++) {
+            if (sp.hasPathTo(t)) {
+                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+                for (DirectedEdge e : sp.pathTo(t)) {
+                    StdOut.print(e + "   ");
+                }
+                StdOut.println();
+            }
+            else {
+                StdOut.printf("%d to %d         no path\n", s, t);
+            }
+        }
     }
 }
