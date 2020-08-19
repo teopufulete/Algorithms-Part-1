3f5fc1c20abc1dc46c1dfa28d404959753f198a6
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-19 12:35:43 +0300

    initialize pq from the array of keys

--- Priority Queues/MinPQ.java
+++ Priority Queues/MinPQ.java
@@ -33,7 +33,13 @@ public class MinPQ<Key> implements Iterable<Key> {
 
     // initialize priority queue from the array of keys
     public MinPQ(Key[] keys) {
-    
+        n = keys.length;
+        pq = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < n; i++)
+            pq[i+1] = keys[i];
+        for (int k = n/2; k >= 1; k--)
+            sink(k);
+        assert isMinHeap();
     }
 
 
