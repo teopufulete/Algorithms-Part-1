e1330ed6fe28477088b84a33ddcfa28c3aeeae97
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-08-27 14:42:44 +0300

    test

--- Priority Queues/MinPQ.java
+++ Priority Queues/MinPQ.java
@@ -183,6 +183,12 @@ public class MinPQ<Key> implements Iterable<Key> {
 
     // unit test
     public static void main(String[] args) {
-
+        MinPQ<String> pq = new MinPQ<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) pq.insert(item);
+            else if (!pq.isEmpty()) StdOut.print(pq.delMin() + " ");
+        }
+        StdOut.println("(" + pq.size() + " left on pq)");
     }
 }
